979507f18949f68ef749d85f1a1977b2ef44d1be
Koray Ozkal <koryozkal@gmail.com>
2020-04-08 03:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function deleteFromObjectByKey(object, key){
   var newObj =  Object.assign({},object)
   delete newObj[key]
   return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
